9b40c9a3467b7a1c58f9058495bb695e9676250a
Dean Huffnagle <HuffnagleDean@gmail.com>
2021-03-23 01:28:18 -0400

    56:11 Graphql started.

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,16 +1,40 @@
-import { MikroORM } from "@mikro-orm/core"
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import mikroConfig from './mikro-orm.config'
+import mikroConfig from "./mikro-orm.config";
+import  express  from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
+    await orm.getMigrator().up();
+    
+    const app = express();
+    // app.get('/', (_, res) => {
+    //     res.send("hello");
+    // })
 
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post);
-    console.log('---------sql 2------------')
-    await orm.em.nativeInsert (Post, {title: 'my first post 2'})
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({em: orm.em})
+    })
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000")
+    })
+   
 };
 
 
-main()
+main().catch((err) => {
+    console.error(err);
+});
---
+++ lireddit-server/src/resolvers/post.ts
@@ -0,0 +1,14 @@
+
+
+import { Post } from "../entities/Post";
+import { MyContext } from "src/types";
+import { Resolver, Query, Ctx } from "type-graphql";
+
+@Resolver()
+export class PostResolver {
+    @Query(() => [Post])
+    posts(@Ctx() {em}: MyContext): Promise<Post[]> {
+        return em.find(Post, {});
+    }
+
+}
