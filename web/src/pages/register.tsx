aa45924da9011d1187f5990d2eb41f7f5a5fca00
Dean Huffnagle <HuffnagleDean@gmail.com>
2021-03-25 17:56:29 -0400

    renamed folders.

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import {
+	FormControl,
+	FormLabel,
+	Input,
+	FormErrorMessage,
+	Box,
+	Button,
+} from '@chakra-ui/react';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+	return (
+		<Wrapper variant="small">
+			<Formik
+				initialValues={{ username: '', password: '' }}
+				onSubmit={(values) => {
+					console.log(values);
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name="username"
+							placeholder="username"
+							label="Username:"
+						/>
+						<Box mt="4">
+							<InputField
+								name="password"
+								placeholder="password"
+								label="Password:"
+								type="password"
+							/>
+						</Box>
+						<Button
+							mt="4"
+							isLoading={isSubmitting}
+							type="submit"
+							color="white"
+							backgroundColor="teal"
+						>
+							register
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
+};
+
+export default Register;
