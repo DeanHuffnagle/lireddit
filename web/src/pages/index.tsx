f6d2d765ee0ba691d2ce12c44a684543808f4840
Dean Huffnagle <HuffnagleDean@gmail.com>
2021-03-26 23:06:58 -0400

    Added server side rendering to the home page.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,21 @@
 import { NavBar } from '../components/NavBar';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { usePostsQuery } from '../generated/graphql';
+const Index = () => {
+	const [{ data }] = usePostsQuery();
+	return (
+		<>
+			<NavBar />
+			<div>hello world</div>
+			<br />
+			{!data ? (
+				<div>Loading...</div>
+			) : (
+				data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+			)}
+		</>
+	);
+};
 
-const Index = () => (
-	<>
-		<NavBar />
-		<div>hello world</div>
-	</>
-);
-
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
